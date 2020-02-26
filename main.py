1443c601c8f98928e30a1943be954178180a859c
Dexie <sheepareawesome777@gmail.com>
2020-02-26 15:03:09 +0000

    l e t t u c e

--- main.py
+++ main.py
@@ -5,17 +5,30 @@ import replit
 
 sub = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
 
+prefixes = [
+	"YourMom",
+	"Mon",
+	"Di",
+	"Tri",
+	"Tetra",
+	"Penta",
+	"Hexa",
+	"Hepta",
+	"Octa",
+]
+
 metals = [
 	{"name": "Lithium", "ve": 1},
 	{"name": "Beryllium", "ve": 2}
 ]
 
 nonmetals = [
-	#
+	{"name":"Oxygen", "ve":6}
 ]
 
 def makeBond():
-	pass
+	m = choice(metals)
+	nm = choice(nonmetals)
 
 print(colored("Welcome to Nanoframe!\n" \
 			"Press enter to make a bond.", "red")); input()
